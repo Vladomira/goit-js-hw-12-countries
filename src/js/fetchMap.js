9a509e2e655e9ef769be9f812e2f896542f7ef92
Vladomira <juliavladomira@gmail.com>
2021-08-13 16:45:03 +0300

    corrected

--- src/js/fetchMap.js
+++ src/js/fetchMap.js
@@ -7,10 +7,10 @@
 //       .catch(error => error)  
 // }
 
-https://pcmiler.alk.com/apis/rest/v1.0/service.svc/map
-export default function fetchMap(county){
-    return fetch( `https://pcmiler.alk.com/apis/rest/v1.0/service.svc/${map}`)
-      .then(response => response.json())
-      .then(maps => maps)
-      .catch(error => error)  
-}
+// https://pcmiler.alk.com/apis/rest/v1.0/service.svc/map
+// export default function fetchMap(county){
+//     return fetch( `https://pcmiler.alk.com/apis/rest/v1.0/service.svc/${map}`)
+//       .then(response => response.json())
+//       .then(maps => maps)
+//       .catch(error => error)  
+// }
